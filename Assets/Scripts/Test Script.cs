826f53d9d7731f38237ed2c5cfbc8ade236642de
EQwon <bluetwintail@naver.com>
2017-03-09 01:38:57 +0900

    버튼 누르면 되도록 만들자!

---
+++ Assets/Scripts/Test Script.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public void PartTime(string PartTimeName)
+    {
+        switch (PartTimeName)
+        {
+            case "Farm":
+                break;
+            case "Church":
+                break;
+            case "Black Factroy":
+                break;
+            case "Shrine":
+                break;
+            case "Bar":
+                break;
+            case "Delivery":
+                break;
+            case "Hotel":
+                break;
+            case "Housework":
+                break;
+            case "Parenting":
+                break;
+            case "Mine":
+                break;
+            case "Salon":
+                break;
+            case "Hunter":
+                break;
+            case "Graveyard":
+                break;
+            case "Tutor":
+                break;
+            default:
+                Debug.Log("Something is Wrong at PartTime Fuction");
+                break;
+        }
+    }
+}
