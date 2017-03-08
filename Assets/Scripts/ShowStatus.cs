04e0d05eda92e830496ed0184a5691fcbe99d0d7
EQwon <bluetwintail@naver.com>
2017-03-09 03:37:06 +0900

    아르바이트 중 스트레스만 올라가도록 함

---
+++ Assets/Scripts/ShowStatus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowStatus : MonoBehaviour
+{
+    public Text[] StateText;
+
+    private KaramatsuManager KaraManager;
+
+    private void Start()
+    {
+        KaraManager = GetComponent<KaramatsuManager>();
+    }
+
+    public void UpdateStatus()
+    {
+        for (int i = 0; i < 21; i++)
+        {
+            StateText[i].text = KaraManager.Status[i].ToString();
+        }
+    }
+}
--- Assets/Scripts/WorkPartTime.cs
+++ Assets/Scripts/WorkPartTime.cs
@@ -2,39 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class WorkPartTime : MonoBehaviour
 {
+    private KaramatsuManager KaraManager;
+
+    private void Start()
+    {
+        KaraManager = GetComponent<KaramatsuManager>();
+    }
+
     public void PartTime(string PartTimeName)
     {
         switch (PartTimeName)
         {
             case "Farm":
+                KaraManager.Status[0] += 4;
                 break;
             case "Church":
+                KaraManager.Status[0] += 2;
                 break;
-            case "Black Factroy":
+            case "Black Factory":
+                KaraManager.Status[0] += 20;
                 break;
             case "Shrine":
+                KaraManager.Status[0] += 2;
                 break;
             case "Bar":
+                KaraManager.Status[0] += 5;
                 break;
             case "Delivery":
+                KaraManager.Status[0] += 5;
                 break;
             case "Hotel":
+                KaraManager.Status[0] += 3;
                 break;
             case "Housework":
+                KaraManager.Status[0] += 1;
                 break;
             case "Parenting":
+                KaraManager.Status[0] += 3;
                 break;
             case "Mine":
+                KaraManager.Status[0] += 5;
                 break;
             case "Salon":
+                KaraManager.Status[0] += 4;
                 break;
             case "Hunter":
+                KaraManager.Status[0] += 3;
                 break;
             case "Graveyard":
+                KaraManager.Status[0] += 4;
                 break;
             case "Tutor":
+                KaraManager.Status[0] += 3;
                 break;
             default:
                 Debug.Log("Something is Wrong at PartTime Fuction");
