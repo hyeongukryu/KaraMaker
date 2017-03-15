2f22c372a51d89f8f202f241b007b72f110f6dcf
EQwon <bluetwintail@naver.com>
2017-03-16 05:06:49 +0900

    으아 조까튼 UI 비율 조정...

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+	public GameObject ScheduleIconPanel;
+	public GameObject ScheduleMenu;
+	public GameObject Profile;
+	public GameObject DateDisplay;
+	public GameObject Calendar;
+	public GameObject StatusIndex;
+	public GameObject PartTimeJobIndex;
+	public GameObject EducationIndex;
+
+	private void Start()
+	{
+		ScheduleIconPanel.SetActive(true);
+		ScheduleMenu.SetActive(false);
+		Profile.SetActive(true);
+		DateDisplay.SetActive(true);
+		Calendar.SetActive(false);
+		StatusIndex.SetActive(false);
+		PartTimeJobIndex.SetActive(false);
+		EducationIndex.SetActive(false);
+	}
+}
