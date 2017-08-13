8a667b75e7517238cae55c4271f8a657e0293c2d
EQwon <bluetwintail@naver.com>
2017-08-13 16:39:53 +0900

    달력에 날짜 표시 제대로 되도록 함

---
+++ Assets/Scripts/Main/CalendarMonthShower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalendarMonthShower : MonoBehaviour
+{
+	private int month;
+
+	private void Update()
+	{
+		month = DayManager.Month;
+
+		GetComponent<Text>().text = month.ToString();
+	}
+}
