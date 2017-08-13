3e05a81a3ebc4c2342702eaf53e13b6130308902
EQwon <bluetwintail@naver.com>
2017-08-13 16:58:32 +0900

    시작화면 UI 제거 구현

---
+++ Assets/Scripts/Start/StartUIController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartUIController : MonoBehaviour
+{
+	public GameObject[] StartUI;
+	public GameObject UIOnButton;
+
+	private void Start()
+	{
+		foreach(GameObject UI in StartUI)
+		{
+			UI.SetActive(true);
+		}
+
+		UIOnButton.SetActive(false);
+	}
+
+	public void UIOff()
+	{
+		foreach(GameObject UI in StartUI)
+		{
+			UI.SetActive(false);
+		}
+
+		UIOnButton.SetActive(true);
+	}
+
+	public void UIOn()
+	{
+		foreach(GameObject UI in StartUI)
+		{
+			UI.SetActive(true);
+		}
+
+		UIOnButton.SetActive(false);
+	}
+}
