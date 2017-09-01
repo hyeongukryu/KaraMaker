81f752760b699c2303dd276111b16941025f84e7
류형욱 <mintrupt@mintrupt.com>
2017-09-01 12:20:37 +0900

    날짜 및 배경 초기화

---
+++ Unity/Assets/Scripts/Contents/MainInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Game;
+
+namespace Contents
+{
+    class MainInitializer : InitializerBase
+    {
+        public MainInitializer(GameConfiguration gameConfiguration, PlayState playState) : base(gameConfiguration, playState)
+        {
+        }
+        
+        public override void Initialize()
+        {
+            InitializeCalendar();
+        }
+
+        private void InitializeCalendar()
+        {
+            PlayState.Year = 625;
+            PlayState.Month = 1;
+            PlayState.Date = 1;
+            PlayState.Day = 2;
+
+            PlayState.BackgroundKey = "Backgrounds/Main";
+        }
+    }
+}
