ed26eeb00f4536901b22bd2d8e2f02c2e8785258
류형욱 <mintrupt@mintrupt.com>
2017-09-01 10:23:16 +0900

    KaraMakerSheetTools

---
+++ KaraMakerTools/KaraMakerSheetTools/Entity.cs
@@ -0,0 +1,36 @@
+﻿namespace KaraMakerSheetTools
+{
+    public class Entity
+    {
+        public string Key { get; set; }
+        public string ParentKey { get; set; }
+        
+        public string Tag { get; set; }
+        
+        public string DisplayName { get; set; }
+
+        public int MinimumAge { get; set; }
+
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public int Init { get; set; }
+        
+        public string ProbModelKey { get; set; }
+
+        public string ChangeKey { get; set; }
+        public int ChangeAmount { get; set; }
+        
+        public int Gold { get; set; }
+        
+        public string DialogText { get; set; }
+        public bool ShowChanges { get; set; }
+
+        public string Comment { get; set; }
+        
+        public string ImageIdentifier { get; set; }
+        public int AnimationDuration  { get; set; }
+
+        // 스토리 진행
+        public string NextKey { get; set; }
+    }
+}
---
+++ KaraMakerTools/KaraMakerSheetTools/EntityValidator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaraMakerSheetTools
+{
+    static class EntityValidator
+    {
+        public static void Validate(this Entity entity, List<Entity> entities)
+        {
+            if (string.IsNullOrWhiteSpace(entity.Key))
+            {
+                throw new Exception("Key");
+            }
+            if (!string.IsNullOrWhiteSpace(entity.NextKey) &&
+                !entities.Exists(e => e.Key == entity.NextKey))
+            {
+                throw new Exception("NextKey");
+            }
+            if (!string.IsNullOrWhiteSpace(entity.ParentKey) &&
+                !entities.Exists(e => e.Key == entity.ParentKey))
+            {
+                throw new Exception("ParentKey");
+            }
+        }
+
+        public static void Validate(this List<Entity> entities)
+        {
+            foreach (var e in entities)
+            {
+                e.Validate(entities);
+            }
+
+            var keys = from e in entities
+                       select e.Key;
+
+            if (entities.Count != keys.Distinct().Count())
+            {
+                throw new Exception("중복된 키가 있습니다.");
+            }
+        }
+    }
+}
