d308aa5d06cb21fd26710534b88efb20d0614481
류형욱 <mintrupt@mintrupt.com>
2017-09-03 03:43:17 +0900

    스케줄 대사 변환 스크립트

---
+++ KaraConvertScheduleDialog.py
@@ -0,0 +1,498 @@
+def spl(arr):
+    return arr.split('\n')
+
+a = '''ArtBegin
+ArtEnd
+ArtSuccess
+AthleticBegin
+AthleticEnd
+AthleticSuccess
+BarBegin
+BarEndA
+BarEndB
+BarEndC
+BarFail
+BarSuccess
+BlackfactoryBegin
+BlackfactoryEnd
+BlackfactorySuccess
+ChurchBegin
+ChurchEndA
+ChurchEndB
+ChurchEndC
+ChurchFail
+ChurchSuccess
+DanceBegin
+DanceEnd
+DanceSuccess
+DeliveryBegin
+DeliveryEndA
+DeliveryEndB
+DeliveryEndC
+DeliveryFail
+DeliverySuccess
+EtiquetteBegin
+EtiquetteEnd
+EtiquetteSuccess
+FarmBegin
+FarmEndA
+FarmEndB
+FarmEndC
+FarmFail
+FarmSuccess
+FenceBegin
+FenceEnd
+FenceSuccess
+GraveyardBegin
+GraveyardEndA
+GraveyardEndB
+GraveyardEndC
+GraveyardFail
+GraveyardSuccess
+HotelBegin
+HotelEndA
+HotelEndB
+HotelEndC
+HotelFail
+HotelSuccess
+HouseworkBegin
+HouseworkEndA
+HouseworkEndB
+HouseworkEndC
+HouseworkFail
+HouseworkSuccess
+HunterBegin
+HunterEndA
+HunterEndB
+HunterEndC
+HunterFail
+HunterSuccess
+IzakayaBegin
+IzakayaEndA
+IzakayaEndB
+IzakayaEndC
+IzakayaFail
+IzakayaSuccess
+LiteratureBegin
+LiteratureEnd
+LiteratureSuccess
+MagicBegin
+MagicEnd
+MagicSuccess
+MineBegin
+MineEndA
+MineEndB
+MineEndC
+MineFail
+MineSuccess
+ParentingBegin
+ParentingEndA
+ParentingEndB
+ParentingEndC
+ParentingFail
+ParentingSuccess
+RestBegin
+RestEnd
+RestSuccess
+SalonBegin
+SalonEndA
+SalonEndB
+SalonEndC
+SalonFail
+SalonSuccess
+ScienceBegin
+ScienceEnd
+ScienceSuccess
+ShrineBegin
+ShrineEndA
+ShrineEndB
+ShrineEndC
+ShrineFail
+ShrineSuccess
+TheologyBegin
+TheologyEnd
+TheologySuccess
+TutorBegin
+TutorEndA
+TutorEndB
+TutorEndC
+TutorFail
+TutorSuccess
+VacationBegin
+VacationEnd
+VacationSuccess'''
+a = spl(a)
+
+b = '''흐흐.... 저 남자랑 저 남자랑... 이렇게...
+히히.. 그리고 저 남자랑 이 남자랑....
+너무나도 어려운 미술의 세계...
+너희들, 날 따라올 수 있을까?
+오오~ 잘 따라오는데~ 합격!
+열심히 농구를 했다.
+안녕~ 오늘도 손님들 열심히 상대해 줘~
+손님들이 엄청 좋아하더라! 저 재능 있는데? 일단 조금 더 넣어줬어~!
+수고 많았어~ 실수만 조금 더 주의해줘~!
+손님들이 많이 화내던데, 무슨 일 있었어?
+손님들 상대하기 너무 힘들다....
+손님들이 기뻐하는 것 같다!
+...에- 일단 이게 오늘의 작업 일지. 에? 완전 까맣다고? 기분 탓이에요.
+빨리 그만두는 게 좋을 거예요....
+어른이 되어라 - 어른이 되어라 -
+청소하다가 땡땡이 치면 안 돼! 내가 계속 감시하고 있을 테니까~!
+와~ 다시 봤어! 진짜! 다음에도 잘 부탁해♡ 고마워서 조금 더 넣었으니까 감사히 여기라구~!
+수고했어, 이건 약속했던 봉사금!
+오히려 더 더러워졌잖아~~~!!!!
+성당 거울에 비치는 내 모습이 너무 아름다워서 그만....
+성당 청소를 열심히 하는 오레!
+자, 열심히 따라해보라냥!
+수고했다냐~ 정리 깨끗하게 하고 가라냐!
+원투쓰리포, 원투쓰리포.
+우효효효효~ 택배회사에 어서오잔스!
+우효효효~~! 덕분에 돈을 많이 벌었... 크흠, 크흐흠. 수고했잔스. 조금이지만 수고비를 주겠잔스!
+흥, 꽤 쓸만하잔스.
+정말 도움도 안 되는 게 왔잔스.....
+택배가 주인들을 스스로 찾아갔으면 좋겠다......
+오늘도 택배를 주인들에게 전달해주었다.
+예의는 모든 것의 기본이랍니다.
+앉아있느라 고생 많으셨습니다. 다리는 저리지 않나요?
+다리가 좀 저리는 것 같지만 멋진 인간이 되기 위해선...!
+(우오오-! 야구할래? 야구? 에? 야구 아니야?
+와이이-!! 가보로 삼아야제---!!
+수고했사요나라홈런-!
+시무룩.....
+sunshine...을 맞고 있는 나..!
+sunshine을 맞으며 곡식을 열심히 키웠다
+깃발을 잘 쑤시는 게 검술의 기초다죠~!
+다들 깃발과 하나가 되서 기쁘다죠-!
+이 깃발로 다 뚫어버리겠어.
+뭐야, 빌빌되게 생겨선. 기절하는 거 아냐?
+으하하하, 겁쟁이인 줄 알았는데 쓸만하구만. 옛다, 보너스다!
+뭐, 이 정도면 훌륭한 편이지, 키키킥. 잘 가라고, 꼬맹아.
+사실은 기절한 횟수가 더 많은 게 아니냐~?
+(기절한 것 같다....)
+귀신 따위..무섭지...않다!
+할 일은 많지 않으니까 걱정 마.
+덕분에 휴가 딸 수 있을 것 같아. 고마우니 이건 보너스.
+수고했어. 별 일은 없었나 보네.
+무슨 일 있었어? 표정이 안 좋네.
+손님, 대낮부터 뜨거우시네요.....
+흥~흐흐흥~ 흐흥~
+제가 하라는 대로 하시면 되세요!
+와~ 덕분에 한시름 놓았어요! 덕분에 집안 경비를 아낄 수 있었답니다~
+도련님, 고생하셨어요! 이제 가서 쉬세요!
+으, 음... 수고하셨어요. 제가 마무리할 테니 가서 쉬세요.
+평생 부양받으면서 살고 싶네....
+집안일의 신이 날 부르고 있다
+어서오라~~용~~! 어려운 거 아니라~~용!
+훗, 수고 많았다. 이건 수고비다.
+수고많았다~~용~~~!
+밉다~~용~~~! 집에 가라~~용~~~!
+내가 사냥을 하는 건지 사냥을 당하는 건지......
+훗, 영혼들에게 구원을......
+내 오뎅은 세계 제일이라고! 알겠냐!
+오오~ 대단한데, 너 혹시 제자로 들어올 생각 없냐!?
+수고많았다 짜샤, 다음에도 잘 부탁한다고!
+네가 깨먹은 컵이 몇 갠 줄 아냐 짜샤~~~!
+화려한 스탭으로 앞구르기를 했다.
+세계 최고 오뎅, 열심히 서빙을 했다!
+자~ 책 빨리 피렴~ 수업 시작한다!
+오늘 수업은 여기까지~
+흑... 너무나도 아름다운 글이군...
+아~ 파칭코 가고 싶다~ 빨리 시작하자~
+아아~~ 수업 끝~! 그럼 난 파칭코하러 간다~!
+손에서 얼음을 발사하는 마법을 배웠다.
+언젠간 석유를 발견할 수 있을 겁니다!
+오오, 덕분에 새로운 광맥을 찾았습니다. 이건 사례비입니다.
+수고하셨습니다. 조심해서 들어가십시오.
+으, 으음, 이건 대체....
+알고 있어? 석유왕은 사실 금수저라는 것을....
+언젠간 나도 석유왕이 될거야!
+아이가 울거나 하면 빨리 저흴 불러주세요!
+아이들이 참 좋아하는 것 같아요! 약간 더 넣어드렸으니 자주 와 주세요.
+슬슬 일은 익숙해지셨나요?
+아이 다루기 참 힘들죠? 자주 하면 익숙해지실거에요.
+내 자장가가 마음에 안 드는 것인가...
+아이들이 내 말을 잘 들었다
+도련님, 오늘은 쉬는 날이신가요?
+푹 쉬셨나 보네요.
+집에서 뒹굴뒹굴거렸다.
+어서 와~~~ 오늘도 화 이 팅!
+대박이다 어머 어쩌니~~ 진짜 취직해도 되겠어~~
+수고많이했어~ 집 가서 푹 쉬어!
+으음~~~ 네 센스엔 좀 문제가 있는 게 아닐까~~?
+내 미적 감각에 mistake....
+내 센스는 세계 제일이지.
+호에호에, 과학은 아름다운 것이다스.
+호에호에, 고생했다스. 다음에는 더 대단한 걸 배우겠다스!
+이 시약과 이 시약을 섞어서....
+올라오느라 힘들지는 않으셨나요?
+감사합니다, 덕분에 한 시름 놓았어요. 얼마 안 되지만 이건 추가금입니다.
+수고하셨습니다, 다음에 또 와주세요.
+괜찮아요, 다음에 더 잘 하면 되죠.
+신사의 정취를 맡으며 딴 짓을 했다.
+신사의 정취를 맡으며 청소를 열심히 했다.
+신에 대해서 배우면 신께서 소원을 들어주실 거에요.
+다음에도 신에 대해 배워보도록 해요.
+정말로 소원을 들어줄까?
+우리 아이가 마구를 던져서.... 꼭 잘 부탁드리겠습니다.
+오오, 우리 아들의 마구를...! 덕분에 아들도 기뻐하는 것 같군요. 이건 보너스입니다. 정말 감사드립니다.
+수고하셨습니다. 몸은 괜찮으세요?
+허허.... 혹시 다치거나 하진 않으셨습니까?
+날 죽이려는 셈이지!? 그런거지!?!?
+강속구! 받아낼 수 있다!
+준비는 다 되셨나요? 그럼 출발하겠습니다!
+즐거웠죠?
+피서를 즐겼다!'''
+b = spl(b)
+c = '''
+
+ArtArtwork
+
+
+AthleticArtwork
+
+
+
+
+BarFailArtwork
+BarSuccessArtwork
+
+
+BlackfactoryArtwork
+
+
+
+
+ChurchFailArtwork
+ChurchSuccessArtwork
+
+
+DanceArtwork
+
+
+
+
+DeliveryFailArtwork
+DeliverySuccessArtwork
+
+
+EtiquetteArtwork
+
+
+
+
+FarmFailArtwork
+FarmSuccessArtwork
+
+
+FenceArtwork
+
+
+
+
+GraveyardFailArtwork
+GraveyardSuccessArtwork
+
+
+
+
+HotelFailArtwork
+HotelSuccessArtwork
+
+
+
+
+HouseworkFailArtwork
+HouseworkSuccessArtwork
+
+
+
+
+HunterFailArtwork
+HunterSuccessArtwork
+
+
+
+
+IzakayaFailArtwork
+IzakayaSuccessArtwork
+
+
+LiteratureArtwork
+
+
+MagicArtwork
+
+
+
+
+MineFailArtwork
+MineSuccessArtwork
+
+
+
+
+ParentingFailArtwork
+ParentingSuccessArtwork
+
+
+RestArtwork
+
+
+
+
+SalonFailArtwork
+SalonSuccessArtwork
+
+
+ScienceArtwork
+
+
+
+
+ShrineFailArtwork
+ShrineSuccessArtwork
+
+
+TheologyArtwork
+
+
+
+
+TutorFailArtwork
+TutorSuccessArtwork
+
+
+'''
+c = spl(c)
+
+d = '''ArtPortrait
+ArtPortrait
+
+AthleticPortrait
+AthleticPortrait
+
+BarPortrait
+BarPortrait
+BarPortrait
+BarPortrait
+
+
+BlackfactoryPortrait
+
+
+ChurchPortrait
+ChurchPortrait
+ChurchPortrait
+ChurchPortrait
+
+
+DancePortrait
+DancePortrait
+
+DeliveryPortrait
+DeliveryPortrait
+DeliveryPortrait
+DeliveryPortrait
+
+
+EtiquettePortrait
+EtiquettePortrait
+
+FarmPortrait
+FarmPortrait
+FarmPortrait
+FarmPortrait
+
+
+FencePortrait
+FencePortrait
+
+GraveyardPortrait
+GraveyardPortrait
+GraveyardPortrait
+GraveyardPortrait
+
+
+HotelPortrait
+HotelPortrait
+HotelPortrait
+HotelPortrait
+
+
+HouseworkPortrait
+HouseworkPortrait
+HouseworkPortrait
+HouseworkPortrait
+
+
+HunterPortrait
+HunterPortrait
+HunterPortrait
+HunterPortrait
+
+
+IzakayaPortrait
+IzakayaPortrait
+IzakayaPortrait
+IzakayaPortrait
+
+
+LiteraturePortrait
+LiteraturePortrait
+
+MagicPortrait
+MagicPortrait
+
+MinePortrait
+MinePortrait
+MinePortrait
+MinePortrait
+
+
+ParentingPortrait
+ParentingPortrait
+ParentingPortrait
+ParentingPortrait
+
+
+RestPortrait
+RestPortrait
+
+SalonPortrait
+SalonPortrait
+SalonPortrait
+SalonPortrait
+
+
+SciencePortrait
+SciencePortrait
+
+ShrinePortrait
+ShrinePortrait
+ShrinePortrait
+ShrinePortrait
+
+
+TheologyPortrait
+TheologyPortrait
+
+TutorPortrait
+TutorPortrait
+TutorPortrait
+TutorPortrait
+
+
+VacationPortrait
+VacationPortrait
+'''
+d = spl(d)
+
+for i in range(len(d)):
+    c[i] = 'null' if c[i] == '' else '"' + c[i] + '"'
+    d[i] = 'null' if d[i] == '' else '"' + d[i] + '"'
+
+    template = 'addScheduleDialog("{}", "{}", {}, {});'
+    res = template.format(a[i], b[i], c[i], d[i])
+    print(res)
+
